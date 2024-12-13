237180d7b87f96ab4602304dfca1e0f7db4764cf
Dima <edimasya@gmail.com>
2024-12-13 23:38:44 +0100

    corrections

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -2,9 +2,7 @@ package hexlet.code.schemas;
 
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
